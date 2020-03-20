2699fd573c3aef6dc145497716984fbc5702381a
whoIsRicardo <ricardo.santos.0492@gmail.com>
2020-03-20 14:45:48 -0300

    lab2

--- lab2/lab2.m
+++ lab2/lab2.m
@@ -17,14 +17,16 @@ fc = 20*(10^3);
 c = cos(2*pi*fc.*t);
 
 s = m.*c; 
-
+%% a)
 figure(1)
 subplot(3,1,1)
 plot(t,m)
+grid on
 title("m(t)");
 xlim([0.94 0.95]);
 subplot(3,1,2)
 plot(t,c)
+grid on
 title("c(t)");
 xlim([0.94 0.95]);
 
@@ -32,16 +34,41 @@ subplot(3,1,3)
 plot(t,s,'b')
 hold on 
 plot(t,m,'k')
+grid on
 title("s(t) e m(t)"); 
 xlabel("Tempo(s)")
 xlim([0.94 0.95]);
 
+%% b)
+M = abs(fftshift(fft(m)))/N;
+C = abs(fftshift(fft(c)))/N;
+S = abs(fftshift(fft(s)))/N;
+%T=0.05;
+%fs=1/T;
+f = (-fs/2:fs/N:(fs/2-fs/N))./1000;
 
 
 
+figure(2)
+%|M(f)|
+subplot(3,1,1)
+plot(f,M)
+xlim([-4 4])
+%title('Sinal |M(f)|')
+ylabel('M(f)')
 
+subplot(3,1,2)
+plot(f,C)
+ylabel('C(f)')
+xlim([-80 80])
 
+subplot(3,1,3) 
+plot(f,S)
+xlim([-80 80])
+ylabel('S(f)')
+xlabel('f(Hz)')
 
+%%
 
 
 
